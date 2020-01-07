155998f3ca68373be5c54629b31b7b9305e2d3f2
Jordhan <jordhan.rdz@gmail.com>
2020-01-07 09:41:10 -0300

    End of state management

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../widgets/nav_drawer.dart';
 import '../widgets/products_grid.dart';
 import '../widgets/badge.dart';
 import '../providers/cart.dart';
@@ -65,6 +66,7 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
           ),
         ],
       ),
+      drawer: MainDrawer(),
       body: ProductsGrid(_showOnlyFavorites),
     );
   }
